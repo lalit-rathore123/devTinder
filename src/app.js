b5d82d57bfa6f36296077fd41a0cf5b35a74ea22
pradeepmakad <pradeepjangid661@gmail.com>
2026-03-21 13:31:21 +0530

    create testing crud

--- src/app.js
+++ src/app.js
@@ -1,14 +1,35 @@
 const express = require("express");
 const app = express();
+const port = 5000;
 
-app.get("/test", (req, res) => {
-  res.send("hello from test page!");
-});
-app.get("/", (req, res) => {
-  res.send("hello from dashboard!");
+app.get("/user", (req, res) => {
+  res.send({
+    name: "John Doe",
+    email: "john.doe@example.com",
+    age: 30,
+    address: {
+      street: "123 Main St",
+      city: "Anytown",
+      state: "CA",
+      zip: "12345",
+    },
+  });
 });
 
-app.listen(786, () => {
-  console.log("app is listing on port 786");
+app.post("/user", (req, res) => {
+  res.send("User created successfully");
 });
- 
+
+app.put("/user/:id", (req, res) => {
+  res.send(`User with id ${req.params.id} updated successfully`);
+});
+
+app.delete("/user/:id", (req, res) => {
+  res.send(`User with id ${req.params.id} deleted successfully`);
+});
+
+
+
+app.listen(port, () => {
+  console.log(`app is listening on port ${port}`);
+});
